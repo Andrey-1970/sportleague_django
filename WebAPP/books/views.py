22f65e5860a77f689dbe7354aa429b115aa00ad3
Andrey <Andrey_1970@list.ru>
2024-03-16 16:49:00 +0500

    Fix link reverse

--- WebAPP/books/views.py
+++ WebAPP/books/views.py
@@ -1,20 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse
+from django.conf import settings
 import requests
 import json
 
+base_api_url = getattr(settings, 'BASE_API_URL', '')
+
 def index(request):
-    # response = requests.get(reverse(index), auth=('admin', 'admin'))
-    response = requests.get('http://localhost:8000/books', auth=('admin', 'admin'))
+    response = requests.get(f'{base_api_url}/books', auth=('admin', 'admin'))
     if response.status_code == 200:
         return render(request, "books/index.html", {"books": json.loads(response.content)})
     else:
         return HttpResponse('Ошибка при получении данных')
 
 def onebook(request, id):
-    # response = requests.get(reverse(onebook, args=[id]), auth=('admin', 'admin'))
-    response = requests.get(f'http://localhost:8000/books/{id}', auth=('admin', 'admin'))
+    response = requests.get(f'{base_api_url}/books/{id}', auth=('admin', 'admin'))
     if response.status_code == 200:
         return render(request, "books/onebook.html", {"book": json.loads(response.content)})
     else:
